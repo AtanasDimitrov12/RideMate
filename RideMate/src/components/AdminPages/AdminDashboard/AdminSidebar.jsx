ae5a6deb09c4c4c8c83effd87fb7d03d146c4b15
Atanas Dimitrov <530410@student.fontys.nl>
2025-03-15 22:04:43 +0100

    Update admin sidebar

--- RideMate/src/components/AdminPages/AdminDashboard/AdminSidebar.jsx
+++ RideMate/src/components/AdminPages/AdminDashboard/AdminSidebar.jsx
@@ -4,52 +4,32 @@ import { NavLink } from "react-router-dom";
 
 function AdminSidebar() {
   return (
-    <aside className="w-64 bg-white shadow-md">
+    <aside className="w-64 bg-gray-900 text-white h-screen sticky top-0 left-0 p-6">
       <div className="p-4 border-b">
-        <h2 className="text-xl font-bold text-gray-800">Admin Dashboard</h2>
+        <h2 className="text-xl font-bold mb-6">Admin Dashboard</h2>
       </div>
       <nav className="p-4 space-y-2">
         <NavLink
           to="/admin-dashboard/statistics"
-          className={({ isActive }) =>
-            isActive
-              ? "block px-3 py-2 rounded bg-blue-500 text-white"
-              : "block px-3 py-2 rounded hover:bg-gray-100 text-gray-700"
-          }
-        >
+         className={({ isActive }) => `block py-2 px-4 rounded ${isActive ? "bg-blue-500" : "hover:bg-gray-700"}`}>
           Statistics
         </NavLink>
 
         <NavLink
           to="/admin-dashboard/profile"
-          className={({ isActive }) =>
-            isActive
-              ? "block px-3 py-2 rounded bg-blue-500 text-white"
-              : "block px-3 py-2 rounded hover:bg-gray-100 text-gray-700"
-          }
-        >
+         className={({ isActive }) => `block py-2 px-4 rounded ${isActive ? "bg-blue-500" : "hover:bg-gray-700"}`}>
           Profile
         </NavLink>
 
         <NavLink
           to="/admin-dashboard/users"
-          className={({ isActive }) =>
-            isActive
-              ? "block px-3 py-2 rounded bg-blue-500 text-white"
-              : "block px-3 py-2 rounded hover:bg-gray-100 text-gray-700"
-          }
-        >
+          className={({ isActive }) => `block py-2 px-4 rounded ${isActive ? "bg-blue-500" : "hover:bg-gray-700"}`}>
           Manage Users
         </NavLink>
 
         <NavLink
           to="/admin-dashboard/promote-user"
-          className={({ isActive }) =>
-            isActive
-              ? "block px-3 py-2 rounded bg-blue-500 text-white"
-              : "block px-3 py-2 rounded hover:bg-gray-100 text-gray-700"
-          }
-        >
+          className={({ isActive }) => `block py-2 px-4 rounded ${isActive ? "bg-blue-500" : "hover:bg-gray-700"}`}>
           Promote User
         </NavLink>
 
